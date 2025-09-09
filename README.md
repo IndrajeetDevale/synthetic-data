4a87e678230620a4a8984d29da56057807761d7f
IndrajeetDevale <indrajeet.devale@gmail.com>
2025-09-09 19:14:27 -0400

    Initial scaffold: v0.0

---
+++ README.md
@@ -0,0 +1,2 @@
+# Synthetic Data POC
+v0.0 - Project scaffolded. Input -> Bedrock -> Output (stub)
---
+++ src/main.py
@@ -0,0 +1,5 @@
+def main():
+    print("POC started: input -> Bedrock -> output")
+
+if __name__ == "__main__":
+    main()
